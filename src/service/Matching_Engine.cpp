eb17d699171376a3eb7b98e2eed664e96cb2265a
Alexandre <plaisance.alexandre@courrier.uqam.ca>
2026-09-01 23:51:41 -0400

    feat: partial match_buy done, still needs fixes

--- src/service/Matching_Engine.cpp
+++ src/service/Matching_Engine.cpp
@@ -28,4 +28,34 @@ void match_order(std::map<Price, Inventory, std::less<Price>>& sell_book,
   }
 }
 
+void match_buy(std::map<Price, Inventory, std::less<Price>>& sell_book,
+               std::map<Price, Inventory, std::greater<Price>>& buy_book,
+               Order& order) {
+  if (!sell_book.empty()) {
+    while (!sell_book.empty() && sell_book.begin()->first <= order.price &&
+           order.size != 0) {
+      auto best_price_it = sell_book.begin();
+      Inventory& inv = best_price_it->second;
+
+      while (!inv.order_queue.empty()) {
+        order.size -= sell_book.begin()->second.order_queue.front().size;
+        sell_book.begin()->second.quantity -=
+            sell_book.begin()->second.order_queue.front().size;
+        sell_book.begin()->second.order_queue.pop_front();
+        if (sell_book.begin()->second.quantity == 0) {
+          sell_book.erase(sell_book.begin());
+        }
+      }
+      if (sell_book.begin()->first <= order.price && order.size != 0) {
+        sell_book.begin()->second.order_queue.begin()->size -= order.size;
+        sell_book.begin()->second.quantity -= order.size;
+        order.size = 0;
+        return;
+      }
+    }
+    buy_book[order.price].order_queue.push_back(order);
+    buy_book[order.price].quantity += order.size;
+  }
+}
+
 }  // namespace Matching_Engine
