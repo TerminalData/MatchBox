5f7802a383a2d316b6a8f7e1172b77a6e62034e7
Alexandre <plaisance.alexandre@courrier.uqam.ca>
2026-09-02 00:08:42 -0400

    feat: refactored match_buy, fixed it's segfaults

--- src/service/Matching_Engine.cpp
+++ src/service/Matching_Engine.cpp
@@ -31,31 +31,38 @@ void match_order(std::map<Price, Inventory, std::less<Price>>& sell_book,
 void match_buy(std::map<Price, Inventory, std::less<Price>>& sell_book,
                std::map<Price, Inventory, std::greater<Price>>& buy_book,
                Order& order) {
-  if (!sell_book.empty()) {
-    while (!sell_book.empty() && sell_book.begin()->first <= order.price &&
-           order.size != 0) {
-      auto best_price_it = sell_book.begin();
-      Inventory& inv = best_price_it->second;
+  // If there's a matching price, sell
+  while (!sell_book.empty() && sell_book.begin()->first <= order.price &&
+         order.size != 0) {
+    auto best_price_it = sell_book.begin();
+    Inventory& inv = best_price_it->second;
 
-      while (!inv.order_queue.empty()) {
-        order.size -= sell_book.begin()->second.order_queue.front().size;
-        sell_book.begin()->second.quantity -=
-            sell_book.begin()->second.order_queue.front().size;
-        sell_book.begin()->second.order_queue.pop_front();
-        if (sell_book.begin()->second.quantity == 0) {
-          sell_book.erase(sell_book.begin());
-        }
-      }
-      if (sell_book.begin()->first <= order.price && order.size != 0) {
-        sell_book.begin()->second.order_queue.begin()->size -= order.size;
-        sell_book.begin()->second.quantity -= order.size;
+    while (!inv.order_queue.empty() && order.size > 0) {
+      Order& next_to_sell = inv.order_queue.front();
+
+      if (order.size >= next_to_sell.size) {
+        order.size -= next_to_sell.size;
+        inv.quantity -= next_to_sell.size;
+        inv.order_queue.pop_front();
+      } else {
+        next_to_sell.size -= order.size;
+        inv.quantity -= order.size;
         order.size = 0;
         return;
       }
     }
-    buy_book[order.price].order_queue.push_back(order);
-    buy_book[order.price].quantity += order.size;
+    if (inv.order_queue.empty()) {
+      sell_book.erase(best_price_it);
+    }
+  }
+  // If there's not match, creates an order on the buy_book
+  if (order.size > 0) {
+    auto& inv = buy_book[order.price];
+    inv.order_queue.push_back(order);
+    inv.quantity += order.size;
   }
 }
+
+// TODO: implement match_sell based on match_buy
 
 }  // namespace Matching_Engine
